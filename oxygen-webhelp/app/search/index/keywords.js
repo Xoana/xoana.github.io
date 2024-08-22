85dd536cf4f87ac650da2bfdd5d022f85a74a242
Xoana <jana@janaowens.com>
2024-08-22 14:36:43 -0500

    Add dev guide.

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,19 @@
+var keywords=[{w:"Build",p:["p0"]},{w:"and",p:["p0","p5"]},{w:"Deploy",p:["p0"]},{w:"the",p:["p0","p1","p5","p9"]},{w:"UI",p:["p0","p5","p9"]},{w:"Clone",p:["p1"]},{w:"nexus-6-ui",p:["p1"]},{w:"Repository",p:["p1"]},{w:"Create",p:["p2"]},{w:"a",p:["p2"]},{w:"Virtual",p:["p2"]},{w:"Environment",p:["p2","p10"]},{w:"Context",p:["p3"]},{w:"Sensitive",p:["p3"]},{w:"Help",p:["p3"]},{w:"Install",p:["p4","p5"]},{w:"duo-sso",p:["p4"]},{w:"Run",p:["p5"]},{w:"on",p:["p5"]},{w:"localhost",p:["p5"]},{w:"Knowledge",p:["p6"]},{w:"Requirements",p:["p6","p11"]},{w:"Overview",p:["p7"]},{w:"Prerequisites",p:["p8"]},{w:"Release",p:["p9"]},{w:"Nexus-6",p:["p9"]},{w:"Set",p:["p10"]},{w:"Up",p:["p10"]},{w:"Your",p:["p10"]},{w:"Local",p:["p10"]},{w:"Software",p:["p11"]},{w:"Source",p:["p12"]},{w:"Code",p:["p12"]},{w:"Components",p:["p12"]}];
+var ph={};
+ph["p0"]=[0, 1, 2, 3, 4];
+ph["p1"]=[5, 3, 6, 7];
+ph["p2"]=[8, 9, 10, 11];
+ph["p3"]=[12, 13, 14];
+ph["p4"]=[15, 16];
+ph["p5"]=[15, 1, 17, 3, 4, 18, 19];
+ph["p6"]=[20, 21];
+ph["p7"]=[22];
+ph["p8"]=[23];
+ph["p9"]=[24, 3, 25, 4];
+ph["p10"]=[26, 27, 28, 29, 11];
+ph["p12"]=[31, 32, 33];
+ph["p11"]=[30, 21];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
